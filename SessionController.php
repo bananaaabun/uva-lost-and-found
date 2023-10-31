8460e3db1b8d9015c37ba8e1e8c8a396178264ac
Nate Gleberman <njg7hy@virginia.edu>
2023-10-31 12:26:07 -0400

    login-php small changes to messages

--- SessionController.php
+++ SessionController.php
@@ -46,18 +46,12 @@ class SessionController {
 
     }
 
-    public function showLogin() {
-        $message = "";
-        if (!empty($this->errorMessage))
-            $message .= "<p class='alert alert-danger'>".$this->errorMessage."</p>";
-        include("login.php");
-    }
-
     /**
      * Handle user registration and log-in
      */
     public function login() {
-        $message = "";
+
+        $_SESSION["message"] = "";
 
         include("helper/regex-checks.php");
 
@@ -103,7 +97,7 @@ class SessionController {
             return;
         }
         // If something went wrong, show the welcome page again
-        $_SESSION["message"] = "Something went wrong. Please try again in a second.";
+        $_SESSION["message"] = "Something went wrong. Please refresh the page and try again.";
         header("Location: login.php");
         return;
     }
--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 <?php
 
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 session_start();
 
 ?>
