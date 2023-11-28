1636c200b7553c20ef2c6c7f32f98a8a0d8f66e1
Nate Gleberman <njg7hy@virginia.edu>
2023-11-28 14:02:23 -0500

    add delete button - not function

--- components/account.php
+++ components/account.php
@@ -3,6 +3,12 @@
     <img src="assets/logo.png" style="width: 200px;" alt="logo">
 </div>
 
+<script>
+    function deleteItem(id) {
+        console.log(id);
+    }
+</script>
+
 <?php include("modAccountForm.php"); 
 
 // print_r($_SESSION["user_items"]);
@@ -15,7 +21,22 @@
 
         $itemCount = sizeof($_SESSION["user_items"]);
 
-        echo "You have {$itemCount} item(s)";
+        echo "<p>You have {$itemCount} item(s)</p><br><div class=\"d-flex flex-row flex-wrap justify-content-center\">";
+
+        for ($i = 0; $i < sizeof($_SESSION["user_items"]); $i += 1) {
+            $cur_item = $_SESSION["user_items"][$i];
+            $item_id = $cur_item["item_id"];
+            $claim_status = $cur_item["claim_status"];
+            echo "  
+                <div class=\"fc center outline\">
+                    <h4 class=\"item-name\" style=\"font-weight:bold;\">{$cur_item["item_name"]}</h4>
+                    <p>Status: {$claim_status} </p>
+                    <button class=\"btn btn-danger\">Delete Item</button>
+                </div>
+                ";
+        }
+
+        echo `</div>`;
 
     } else { ?>
 
